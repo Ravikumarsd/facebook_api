3dd4a38e73cdc91da56d8b5298e2f526262d6a76
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-09 22:16:18 +0530

    PSID updated

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
       app = express().use(bodyParser.json()).use(cors()),
-      PORT = process.env.PORT
-
+      PORT = process.env.PORT,
+      PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 app.get('/',(req,res) => res.json("Backend is working properly"))
 
-app.post('/webhook',(req,res)=>{
+app.post('/webhook', (req,res) =>{
       let body = req.body;
       if(body.object === 'page') {
             body.entry.forEach(entry => {
+                  // Gets the body of the webhook event
                   let webhook_event = entry.messaging[0]
                   console.log(webhook_event)
+
+                  //Get the sender PSID
+                  let sender_psid = webhook_event.sender.id
+                  console.log('Sender PSID: ' + sender_psid)
             })
-      res.status(200).json('EVENT_RECEIVED')
+      res.status(200).send('EVENT_RECEIVED')
    } else {
          res.sendStatus(404) //not found
    }
 })
 
 
-app.get('/webhook',(req,res)=>{
+app.get('/webhook',(req,res) => {
       let VERIFY_TOKEN = "chatbot"
-
       let mode = req.query['hub.mode']
       let token = req.query['hub.verify_token']
       let challenge = req.query['hub.challenge']
@@ -39,5 +43,49 @@ app.get('/webhook',(req,res)=>{
                   res.sendStatus(403) //forbidden
             }
       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
 app.listen(PORT || 3000, ()=> console.log(`server is running on port ${PORT}`))
+
+// Handles messages events 
+ handleMessage = (sender_psid, received_message) => {
+ 
+}
+
+
+// Handles messaging_postbacks events
+ handlePostback = (sender_psid, received_postback) => {
+
+}
+
+
+// Sends response messages via the Send API
+ callSendAPI = (sender_psid, response) => {
+  
+}
