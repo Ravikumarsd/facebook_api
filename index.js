3a0354e998ee2cbca1be5fe983676f5636a97044
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-09 23:20:07 +0530

    page_access_token changed

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
+      request = require('request'),
       app = express().use(bodyParser.json()).use(cors()),
       PORT = process.env.PORT,
-      PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+      PAGE_ACCESS_TOKEN = "EAAHp9ZATUmfsBACup51Q2CZA78hekxGtjyNaugSb6uV3N4Al8kF70ZBLWR9nPpP2b0WI1GXBZA9s2tfyjZBtj91cGQkBSMlkkyPlVK4bIE3eoacCvTunBYZCfjC4ZC0ZBYrmDVGGI3JpWJEIcckEORJPnpMVdosBbRqZBcEOHELtdC8CrVEcl9qwPlWmCVJ7dqQdfwoXKhFEyagZDZD"
 
-app.get('/',(req,res) => res.json("Backend is working properly"))
+app.get('/', (req, res) => res.json("Backend is working properly"))
 
-app.post('/webhook', (req,res) =>{
+app.post('/webhook', (req, res) => {
       let body = req.body;
-      if(body.object === 'page') {
+      if (body.object === 'page') {
             body.entry.forEach(entry => {
                   // Gets the body of the webhook event
                   let webhook_event = entry.messaging[0]
@@ -20,22 +21,28 @@ app.post('/webhook', (req,res) =>{
                   //Get the sender PSID
                   let sender_psid = webhook_event.sender.id
                   console.log('Sender PSID: ' + sender_psid)
+
+                  if (webhook_event.message) {
+                        handleMessage(sender_psid, webhook_event.message)
+                  } else if (webhook_event.postback) {
+                        handlePostback(sender_psid, webhook_event.postback);
+                  }
             })
-      res.status(200).send('EVENT_RECEIVED')
-   } else {
-         res.sendStatus(404) //not found
-   }
+            res.status(200).send('EVENT_RECEIVED')
+      } else {
+            res.sendStatus(404) //not found
+      }
 })
 
 
-app.get('/webhook',(req,res) => {
+app.get('/webhook', (req, res) => {
       let VERIFY_TOKEN = "chatbot"
       let mode = req.query['hub.mode']
       let token = req.query['hub.verify_token']
       let challenge = req.query['hub.challenge']
 
-      if(mode && token) {
-            if(mode === 'subscribe' && token === VERIFY_TOKEN) {
+      if (mode && token) {
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
                   console.log('WEBHOOK_VERIFIED');
                   res.status(200).send(challenge);
@@ -71,21 +78,47 @@ app.get('/webhook',(req,res) => {
 
 })
 
-app.listen(PORT || 3000, ()=> console.log(`server is running on port ${PORT}`))
+app.listen(PORT || 3000, () => console.log(`server is running on port ${PORT}`))
 
-// Handles messages events 
-//  handleMessage = (sender_psid, received_message) => {
- 
-// }
+//Handles messages events 
+handleMessage = (sender_psid, received_message) => {
+      let response;
+
+      if(received_message.text) {
+            response = {
+                  "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            }
+      }
+      callSendAPI(sender_psid, response);
+}
 
 
-// // Handles messaging_postbacks events
-//  handlePostback = (sender_psid, received_postback) => {
+// Handles messaging_postbacks events
+handlePostback = (sender_psid, received_postback) => {
 
-// }
+}
 
 
-// // Sends response messages via the Send API
-//  callSendAPI = (sender_psid, response) => {
-  
-// }
+// Sends response messages via the Send API
+callSendAPI = (sender_psid, response) => {
+      //Construct the message body
+      let request_body = {
+            "recipient": {
+                  "id": sender_psid
+            },
+            "message":response
+      }
+      // Send the HTTP request to the Messenger Platform
+  request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  }
