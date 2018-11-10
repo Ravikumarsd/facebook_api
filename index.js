73feb0fe25bdfbeac364179530b9debf70863097
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-10 14:57:27 +0530

    h

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ const callSendAPI=(sender_psid, response) => {
 //     });
     request({
       "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": PAGE_ACCESS_TOKEN || PAGE_ACCESS_TOKEN2},
+      "qs": { "access_token": PAGE_ACCESS_TOKEN},
       "method": "POST",
       "json": request_body
     }, (err, res, body) => {
@@ -132,7 +132,20 @@ const callSendAPI=(sender_psid, response) => {
       } else {
         console.error("Unable to send message:" + err);
       }
-    }); 
+    });
+    
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN2},
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    });
 }
 
 
